c9d352ba3fec7fd6c3ce4eaf2064ccac9b0e7e46
Juliano Jorge <julianojorgepj@gmail.com>
2026-08-21 22:55:26 -0300

    Estudando Operadores

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,58 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // Operadores Aritiméticos: + - / * %
+        int numero01 = 10;
+        int numero02 = 20;
+        int resultado = numero01 * numero02;
+        System.out.println(resultado);
+        System.out.println("Valor " + numero01 + numero02);
+
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // Operadores Relacionais: < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+
+        // Operadores Lógicos: && (AND) || (OR) ! (NOT)
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 500;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlayStation = 5000F;
+        boolean isPlayStationCincoCompravel = valorTotalContaCorrente > valorPlayStation || valorTotalContaPoupanca > valorPlayStation;
+        System.out.println("isPlayStationCincoCompravel " + isPlayStationCincoCompravel);
+
+        // Operadores de Atribuição: = += -= *= /= %=
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2; // 3600
+        bonus /= 2; // 1800
+        bonus %= 2; // 0
+        System.out.println(bonus);
+
+        int contador = 0;
+        contador++; //contador = contador + 1
+        contador--; //contador = contador - 1
+        System.out.println(contador);
+
+        int contador2 = 0;
+        System.out.println("contador2 = " + contador++); // Como o ++ está depois ele primeiro executa, depois incrementa, por isso não aparece o resultado correto
+        System.out.println("contador2 = " + contador);
+
+        int contador3 = 0;
+        System.out.println(++contador3);
+    }
+}
